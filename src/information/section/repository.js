3d072a0987b683fcf7f8e05b822be1611938047f
Somonindro <somoroy1290@gmail.com>
2024-01-30 23:36:29 +0600

    Section CRUD backend part done

---
+++ src/information/section/repository.js
@@ -0,0 +1,85 @@
+import { connect } from "../../config/database.js";
+import { HttpError } from "../../config/error-handle.js";
+
+export async function getAll() {
+  const query = "SELECT * FROM sections";
+
+  const client = await connect();
+  const results = await client.query(query);
+  client.release();
+  return results.rows;
+}
+
+export async function saveSection(sections) {
+  const batch = sections.batch;
+  const section = sections.section;
+  const type = sections.type;
+  const room = sections.room;
+  const session = sections.session;
+  const level_term = sections.level_term;
+
+  const query =
+    "INSERT INTO sections (batch, section,type,room,session) VALUES ($1, $2, $3, $4, $5, $6)";
+  const values = [batch, section, type, room, session, level_term];
+
+  const client = await connect();
+  const results = await client.query(query, values);
+  client.release();
+
+  if (results.rowCount <= 0) {
+    throw new HttpError(400, "Insert Failed");
+  } else {
+    return results.rows;
+  }
+}
+
+export async function updateSection(sections) {
+  const batch = sections.batch;
+  const section = sections.section;
+  const type = sections.type;
+  const room = sections.room;
+  const session = sections.session;
+  const level_term = sections.level_term;
+
+  const query = `
+    UPDATE sections
+    SET
+      type = $3,
+      room = $4,
+      session = $5,
+      level_term = $6
+    WHERE batch = $1 AND
+    section = $2
+  `;
+  const values = [batch, section, type, room, session, level_term];
+  console.log(values);
+
+  const client = await connect();
+  const results = await client.query(query, values);
+  client.release();
+
+  if (results.rowCount <= 0) {
+    throw new HttpError(400, "Update Failed");
+  } else {
+    return results.rows;
+  }
+}
+
+export async function removeSection(batch, section) {
+  const query = `
+    DELETE FROM sections
+    WHERE batch = $1 AND
+    section = $2
+  `;
+  const values = [batch, section];
+
+  const client = await connect();
+  const results = await client.query(query, values);
+  client.release();
+
+  if (results.rowCount <= 0) {
+    throw new HttpError(400, "Delete Failed");
+  } else {
+    return results.rows;
+  }
+}
